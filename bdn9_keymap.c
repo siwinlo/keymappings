058e8771eee5e44ee5e938b78a7d5b07ff77d8bb
Siwin Lo <41877484+siwinlo@users.noreply.github.com>
2021-03-25 12:49:07 -0400

    Create bdn9_keymap.c

---
+++ bdn9_keymap.c
@@ -0,0 +1,102 @@
+/* Copyright 2019 Danny Nguyen <danny@keeb.io>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include QMK_KEYBOARD_H
+
+enum encoder_names {
+  _LEFT,
+  _RIGHT,
+};
+
+enum custom_keycodes {
+    TERMINAL,
+    INSPECTOR,
+    EMOJI,
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+    case TERMINAL:
+        if (record->event.pressed) {
+            // when keycode is pressed
+            SEND_STRING(SS_LCTL("`"));
+        } else {
+            // when keycode QMKBEST is released
+        }
+        break;
+
+    case INSPECTOR:
+        if (record->event.pressed) {
+            // when keycode is pressed
+            SEND_STRING(SS_LGUI(SS_LALT("j")));
+        } else {
+            // when keycode QMKURL is released
+        }
+        break;
+
+    case EMOJI:
+        if (record->event.pressed) {
+            // when keycode is pressed
+            SEND_STRING(SS_LGUI(SS_LCTL(" ")));
+        } else {
+            // when keycode QMKURL is released
+        }
+        break;
+
+    }
+    return true;
+};
+
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /*
+        | Knob 1: Vol Dn/Up |      | Knob 2: Page Dn/Up |
+        | Press: Mute       | Home | Press: Play/Pause  |
+        | Hold: Layer 2     | Up   | RGB Mode           |
+        | Left              | Down | Right              |
+     */
+    [0] = LAYOUT(
+        KC_MUTE, MO(1)  , KC_MPLY,
+        TERMINAL, KC_UP  , INSPECTOR,
+        KC_LEFT, KC_DOWN, KC_RGHT
+    ),
+    /*
+        | RESET          | N/A  | Media Stop |
+        | Held: Layer 2  | Home | RGB Mode   |
+        | Media Previous | End  | Media Next |
+     */
+    [1] = LAYOUT(
+        RESET  , _______, KC_STOP,
+        EMOJI, RGB_SAI, RGB_MOD,
+        RGB_HUD, RGB_SAD , RGB_HUI
+    ),
+};
+
+void encoder_update_user(uint8_t index, bool clockwise) {
+    if (index == _LEFT) {
+        if (clockwise) {
+            tap_code(KC_VOLU);
+        } else {
+            tap_code(KC_VOLD);
+        }
+    }
+    else if (index == _RIGHT) {
+        if (clockwise) {
+            tap_code(KC_PGDN);
+        } else {
+            tap_code(KC_PGUP);
+        }
+    }
+}
